52178810d9fc3b50d2f04ac806b4377149d845ce
KonstantinShcherbakov <super228telefon@gmail.com>
2023-01-18 22:04:50 +0200

    lesson 10

---
+++ lesson10/js/main.js
@@ -0,0 +1,69 @@
+function renderObject() {
+	console.log(Object.keys(this));
+	for (var index = 0; index < Object.keys(this).length; index++) {
+		if(Object.keys(this)[index] != "renderObject")
+		{
+			console.log(Object.keys(this)[index] + ': ' + this[Object.keys(this)[index]]);
+		}
+	}
+}
+
+var obj = {
+	x: 10,
+	y: 15,
+	z: 'gay',
+	f: function() {
+		console.log('pepe');
+	}
+}
+
+obj.renderObject = renderObject;
+
+console.log("1 task\n---------------------------------------------");
+
+obj.renderObject();
+
+console.log("---------------------------------------------");
+
+var obj2 = {
+	text: 'its me',
+	m1: function() {
+		console.log('m1 ' + this.text);
+		return this;
+	},
+	m2: function() {
+		console.log('m2');
+		return this;
+	}
+}
+
+console.log("2 task\n---------------------------------------------");
+
+obj2.m1();
+obj2.m2();
+
+obj2.m1().m2();
+
+obj2.m2().m1();
+
+console.log("---------------------------------------------");
+
+console.log("3 task\n---------------------------------------------");
+
+function addRecord() {
+	for (var i = 0; i < arguments.length; i++) {
+		console.log(arguments[i]);
+		for (var j = 0; j < Object.keys(arguments[i]).length; j++) {
+			this[Object.keys(arguments[i])[j]] = arguments[i][Object.keys(arguments[i])[j]];
+		}
+	}
+	console.log(this);
+}
+
+data = {
+	addRecord: addRecord,
+}
+
+data.addRecord({x: 'y',jojo: 5},{papa: 43, x: 53});
+
+console.log("---------------------------------------------");
