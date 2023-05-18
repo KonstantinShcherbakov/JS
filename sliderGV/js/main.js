b0160bc74de68eaa15c6c29edb01b24558966309
buryk20 <73736907+buryk20@users.noreply.github.com>
2023-05-18 09:20:05 +0300

    fix

--- sliderGV/js/main.js
+++ sliderGV/js/main.js
@@ -4,16 +4,29 @@ let currentSliderIndex = 0;
 
 console.log(slider_sections);
 
-function changeSlide(event,index) {
-	if(index!==currentSliderIndex) {
-		slider_sections[currentSliderIndex].classList.remove('slider__section-active');
-		slider_images[currentSliderIndex].classList.remove('slider__active-img');
-		slider_images[index].classList.add('slider__active-img');
-	}
-	slider_sections[index].classList.toggle('slider__section-active');
-	currentSliderIndex = index;
-}
+// function changeSlide(event,index) {
+// 	if(index!==currentSliderIndex) {
+// 		slider_sections[currentSliderIndex].classList.remove('slider__section-active');
+// 		slider_images[currentSliderIndex].classList.remove('slider__active-img');
+// 		slider_images[index].classList.add('active');
+// 	}
+// 	slider_sections[index].classList.toggle('slider__section-active');
+// 	currentSliderIndex = index;
+// 	return
+// }
 
-for (let index = 0; index < slider_sections.length; index++) {
-	slider_sections[index].addEventListener('click', function(){changeSlide(event,index)})
-}
+// for (let index = 0; index < slider_sections.length; index++) {
+// 	slider_sections[index].addEventListener('click', function(){changeSlide(event,index)})
+// }
+
+slider_sections.forEach((el, index) => {
+	el.addEventListener('click', () => {
+		if(index!==currentSliderIndex) {
+			slider_sections[currentSliderIndex].classList.remove('slider__section-active');
+			slider_images[currentSliderIndex].classList.remove('slider__active-img');
+			slider_images[index].classList.add('slider__active-img');
+		}
+		slider_sections[index].classList.toggle('slider__section-active');
+		currentSliderIndex = index;
+	});
+});
