137b4a1df423f3a342d0557b30116b94a68f55b0
KonstantinShcherbakov <super228telefon@gmail.com>
2023-05-22 23:45:10 +0300

    next step

--- sliderGV/js/main.js
+++ sliderGV/js/main.js
@@ -1,26 +1,10 @@
 let slider_sections = document.querySelectorAll('.slider__section');
 let slider_images = document.querySelectorAll('.slider__img');
+let slider_headers = document.querySelectorAll('.slider__section-header-wrp');
 let arrSlid = [slider_sections, slider_images];
 let currentSliderIndex = 0;
 
-// console.log(slider_sections);
-
-// function changeSlide(event,index) {
-// 	if(index!==currentSliderIndex) {
-// 		slider_sections[currentSliderIndex].classList.remove('slider__section-active');
-// 		slider_images[currentSliderIndex].classList.remove('slider__active-img');
-// 		slider_images[index].classList.add('active');
-// 	}
-// 	slider_sections[index].classList.toggle('slider__section-active');
-// 	currentSliderIndex = index;
-// 	return
-// }
-
-// for (let index = 0; index < slider_sections.length; index++) {
-// 	slider_sections[index].addEventListener('click', function(){changeSlide(event,index)})
-// }
-
-slider_sections.forEach((el, index) => {
+slider_headers.forEach((el, index) => {
 	el.addEventListener('click', () => {
 		if(index!==currentSliderIndex) {
 			delActive(arrSlid, currentSliderIndex);
